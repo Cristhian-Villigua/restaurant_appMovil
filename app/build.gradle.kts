d61b9ab60cc45ff02cb15d03d60187dbed731f85
Cristhian-Villigua <cristhian.soled@gmail.com>
2025-10-14 11:24:36 -0500

    Sixth Commit CAVS, Connection DB

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -45,4 +46,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    kapt(libs.room.compiler)
+    implementation(libs.kotlinx.coroutines.android)
 }
---
+++ app/src/main/java/com/example/project_mobileapplicacion/model/UserEntity.kt
@@ -0,0 +1,17 @@
+package com.example.project_mobileapplicacion.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+
+data class UserEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val lastname: String,
+    val birthday: String,
+    val phone: String,
+    val email: String,
+    val password: String
+)
